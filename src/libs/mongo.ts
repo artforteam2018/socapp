b170cf96fc8e397c0733c35be2624e6c99d82763
artforteam <artforteam2015@gmail.com>
2020-11-23 01:58:08 +0300

    'su'

--- src/libs/mongo.ts
+++ src/libs/mongo.ts
@@ -1,6 +1,6 @@
 import * as Mongodb from 'mongoose';
 
-Mongodb.connect(`mongodb://localhost:27017/${process.env.TEST ? 'books_test' : 'books'}`,
+Mongodb.connect(`mongodb://0.0.0.0:27017/${process.env.TEST ? 'books_test' : 'books'}`,
                 { useNewUrlParser: true, useUnifiedTopology: true });
 
 export {
